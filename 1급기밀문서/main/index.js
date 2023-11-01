983f8fa9f01a81af839fcae6a3e0703268a965d2
[SW7] 박건우 <ceh2000212@gmail.com>
2023-11-01 22:45:46 +0900

    Feat 메인 페이지 1차 구현

--- 1급기밀문서/main/index.js
+++ 1급기밀문서/main/index.js
@@ -6,22 +6,26 @@ const vibration = (target) => {
     }, 400);
 }
 function expandSticky() {
-    
+    console.log("expandSticky function called");
+
     if (stickyImg.classList.contains('stick-img-happy')) {
+        console.log("stick-img-happy class found, removing it");
         stickyImg.classList.remove('stick-img-happy');
-        console.log("in");
     } else {
+        console.log("stick-img-happy class not found, adding it");
         stickyImg.classList.add('stick-img-happy');
-        console.log("no");
     }
 }
 
 const mainShoppingList = document.querySelector(".main-container");
 const shoppingStartBtn = document.querySelector(".forSeelist");
 const stickyImg = document.querySelector('.sticky-img');
+const footer = document.querySelector(".footer-box1");
 
 shoppingStartBtn.addEventListener('click',() => {
     mainShoppingList.style.display = "block";
+    footer.style.display = "block"
+
     setTimeout(() => {
         stickyImg.classList.add('stick-img-happy');
     }, 500);

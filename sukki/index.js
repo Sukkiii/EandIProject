be3a4f79c5a29dc0fdf00d0b2a78af0f4be20207
sukki <knk951213@naver.com>
2023-11-03 21:12:36 +0900

    Feat: 인기상품, 일반 상품 데이터 뿌려주기 구현

--- sukki/index.js
+++ sukki/index.js
@@ -1,28 +1,26 @@
-const dummyFile = "./data/dummyData.json";
+const bestProductsFile = "./data/dummyData.json";
+const productsFile = "./data/dummyData2.json";
 
-function fetchProductData() {
-  return (
-    fetch(dummyFile)
-      // return fetch("/api/products")
-      .then((response) => response.json())
-      .catch((error) => {
-        console.error("data error", error);
-      })
-  );
+function fetchProductData(url) {
+  // return fetch("/api/products")
+  return fetch(url)
+    .then((response) => response.json())
+    .catch((error) => {
+      console.error("data error", error);
+    });
 }
 
 function displayProducts(products, containerClass, thumbnailClass) {
   const productUl = document.querySelector(`.${containerClass} .product-ul`);
-  // productUl.innerHTML = "";
 
   products.forEach((product) => {
     const productLi = document.createElement("li");
-    // const productUl = document.querySelector(".product-ul");
     productLi.classList.add("product-li");
 
-    const thumbnailClassAdd = product.isBest
-      ? "best-product-thumbnail"
-      : "product-thumbnail";
+    const thumbnailClassAdd =
+      containerClass === "best-products"
+        ? "best-product-thumbnail"
+        : "product-thumbnail";
     productLi.innerHTML = `
     <div class="${thumbnailClassAdd}">
       <img
@@ -35,7 +33,7 @@ function displayProducts(products, containerClass, thumbnailClass) {
       <div class="product-name">${product.ProductName}</div>
       <div class="product-price">${product.Price}원</div>
       ${
-        product.isBest
+        containerClass === "best-products"
           ? ""
           : `<div class="product-explain">${product.Description}</div>`
       }
@@ -47,13 +45,23 @@ function displayProducts(products, containerClass, thumbnailClass) {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  fetchProductData().then((data) => {
-    displayProducts(
-      data.bestProducts,
-      "best-products",
-      "best-products-thumbnail"
-    );
+  fetchProductData(bestProductsFile)
+    .then((data) => {
+      displayProducts(
+        data.bestProducts,
+        "best-products",
+        "best-products-thumbnail"
+      );
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 
-    displayProducts(data.products, "products", "product-thumbnail");
-  });
+  fetchProductData(productsFile)
+    .then((data) => {
+      displayProducts(data.products, "products", "product-thumbnail");
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 });

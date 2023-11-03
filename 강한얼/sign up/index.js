ac72b3b5cde3b224da719238b12003ffc96b6138
han_ur92 <rkdgksdjfwkd92@gmail.com>
2023-11-04 02:19:41 +0900

    validation test

--- 강한얼/sign up/index.js
+++ 강한얼/sign up/index.js
@@ -1,77 +1,80 @@
-// //각필드가비어있으면버튼을눌렀을때타켓필드에일림+채워지면알림x
-// //비밀번호와비밀번호확인이틀리면필드에알림
-// //약관체크
-// //성별체크
-// //submit누르면각필드의밸류를전달(어떻게?)
-// //  추가기능1. js써서 필드가 다 차면 secondary에서 success로 class변경
-// const signupForm = document.querySelector("#signupForm");
-// const emailInput = document.querySelector("#emailInput");
+//각필드가비어있으면버튼을눌렀을때타켓필드에일림+채워지면알림x
+//비밀번호와비밀번호확인이틀리면필드에알림
+//약관체크
+//성별체크
+//submit누르면각필드의밸류를전달(어떻게?)
+//  추가기능1. js써서 필드가 다 차면 secondary에서 success로 class변경
+const signupForm = document.querySelector("#signupForm");
+const emailInput = document.querySelector("#emailInput");
 
-// const passwordInput = document.querySelector("#passwordInput");
+const passwordInput = document.querySelector("#passwordInput");
 
-// const passwordCheckInput = document.querySelector("#passwordCheckInput");
-// const birthInput = document.querySelector("#birthInput");
-// const addressInput = document.querySelector("#addressInput");
-// const termsButton = document.querySelector("#termsButton");
-// const submitButton = document.querySelector("#submitButton");
+const passwordCheckInput = document.querySelector("#passwordCheckInput");
+const birthInput = document.querySelector("#birthInput");
+const addressInput = document.querySelector("#addressInput");
+const termsButton = document.querySelector("#termsButton");
+const submitButton = document.querySelector("#submitButton");
+const valid = document.querySelector("#valid");
+//id길이검사
+const idLen = (val) => {
+  return val.length >= 4 && val.length <= 12;
+};
 
-// //id길이검사
-// const idLen = (val) => {
-//   return val.length >= 4 && val.length <= 12;
-// };
+//id중복검사
 
-// //id중복검사
+ passwordCheckInput.addEventListener("keyup",pwdOnchange );
+function pwdOnchange() {
+  let div = document.createElement("div");
+  if (passwordInput.value !== passwordCheckInput.value) {
+    div.innerHTML= "일치하지않습니다"
+    valid.appendChild(div);
+ 
+  } else {
+    div.innerHTML = "일치합니다."
+    valid.appendChild(div);
+  }
+}
 
-// //pwd검사 //pwdCheck
+
+//pwd검사 //pwdCheck
 // const pwdCheck = (password1, password2) => {
 //   return password1 === password2;
 // };
-// //  ===>true면 append로 pwdchk밑에 요소추가(일치합니다)
-// //  ===>false면 append로 pwdchk밑에 요소추가 (틀립니다)
+//  ===>true면 append로 pwdchk밑에 요소추가(일치합니다)
+//  ===>false면 append로 pwdchk밑에 요소추가 (틀립니다)
 
-// passwordCheckInput.addEventListener("keyup", function pwdOnchange() {
-//   if (!pwdCheck(passwordInput, passwordCheckInput)) {
-//     let div = document.createElement('div')
-//     let msg  = document.createTextNode('일치하지 않습니다.');
-//     div.appendChild(msg)
-//      document.querySelector(passwordCheckInput).appendChild('div')
- 
-//   }else {
-//     div.innerText = '일치합니다.'
-//   }
-// });
 
-// //버튼누르면정보를백으로뿌려주는최종함수
-// const submitHandler = (e) => {
-//   e.preventDefault();
+//버튼누르면정보를백으로뿌려주는최종함수
+const submitHandler = (e) => {
+  e.preventDefault();
 
-//   signup(
-//     emailInput.value,
-//     passwordInput.value,
-//     birthInput.value,
-//     addressInput.value
-//   );
-// };
+  signup(
+    emailInput.value,
+    passwordInput.value,
+    birthInput.value,
+    addressInput.value
+  );
+};
 
-// submitButton.addEventListener("click", submitHandler);
+submitButton.addEventListener("click", submitHandler);
 
-// //정보를json화하여body에담아보내는함수
-// const signup = async (email, password, birth, address) => {
-//   try {
-//     const response = await fetch("http://localhost:5500/signup", {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify({
-//         email: email,
-//         password: password,
-//         birth: birth,
-//         address: address,
-//       }),
-//     });
-//     console.log(response);
-//   } catch (error) {
-//     console.error("Error:", error);
-//   }
-// };
+//정보를json화하여body에담아보내는함수
+const signup = async (email, password, birth, address) => {
+  try {
+    const response = await fetch("http://localhost:5500/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+        birth: birth,
+        address: address,
+      }),
+    });
+    console.log(response);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+};

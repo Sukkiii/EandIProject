9c38cc046e56b4dc13b27af794b726b08cee2a28
[SW7] 서원식 <seodnjstlr@gmail.com>
2023-11-08 13:11:41 +0000

    Upload New File

---
+++ back-end/routes/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+// const permission = require('../middlewares/permission')
+const usersRouter = require('../routes/users');
+const productsRouter = require('../routes/products');
+const ordersRouter = require('../routes/orders');
+const categoriesRouter = require('../routes/categories');
+const { getProduct, getProductList } = require('../services/productServices');
+const { signup, login, logout } = require('../services/userServices');
+const { getCategory } = require('../services/categoryServices');
+const router = express.Router();
+
+// 로그인&아웃, 회원 가입
+router.post('/signup', signup) // 회원가입
+router.post('/login', login) // 로그인
+router.delete('/logout', logout) // 로그아웃
+
+//메인 페이지 및 상품 조회
+router.get('/', getCategory) // 주문 조회
+router.get('/', getProductList) // 상품 목록 조회
+router.get('/produts/:id', getProduct) // 상품 조회
+
+// 관리자
+router.use('/admin/users', usersRouter)
+router.use('/admin/products', productsRouter)
+router.use('/admin/orders', ordersRouter)
+router.use('/admin/categories', categoriesRouter)
+// 판매자
+router.use('/seller/users', usersRouter)
+router.use('/seller/products', productsRouter)
+router.use('/seller/orders', ordersRouter)
+router.use('/seller/categories', categoriesRouter)
+// 유저
+router.use('/user/users', usersRouter)
+router.use('/user/products', productsRouter)
+router.use('/user/orders', ordersRouter)
+router.use('/user/categories', categoriesRouter)
+
+module.exports = router;

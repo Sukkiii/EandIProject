a6789bf9b73ad443127431af8ec5cc0da6b1c840
[SW7] 서원식 <seodnjstlr@gmail.com>
2023-11-09 08:06:30 +0000

    Update orders.js

--- routes/orders.js
+++ routes/orders.js
@@ -1,13 +1,15 @@
 const express = require('express');
-const { getOrder, createOrder, updateOrder, deleteOrder, getOrderList, getOrders } = require('../services/orderServices')
+const { getOrder, createOrder, updateOrder, deleteOrder, updateDeliveryStatus, getOrderList, getOrders } = require('../services/orderServices')
+const permission = require('./middlewares/permission'); // 유저인증 & 권한 체크
 const router = express.Router();
 
 // 주문은 app.js에서 미리 유저 체크
 router.get('/', getOrderList) // 전체 주문 목록 조회
-router.get('/:userId', getOrders) // 주문 목록
+router.get('/:userId', permission('admin'), getOrders) // 주문 목록
 router.get('/:id', getOrder) // 주문 조회
 router.post('/:id', createOrder) // 주문 추가(완료)
 router.put('/:id', updateOrder) // 주문 수정
 router.delete('/:id', deleteOrder) // 주문 삭제
+router.put('/:orderId', permission('admin'), updateDeliveryStatus) // 배송 상태 수정
 
 module.exports = router;

46593deafcba7e58dcdfcb8895edd86cbc115f1e
[SW7] 서원식 <seodnjstlr@gmail.com>
2023-11-09 08:04:11 +0000

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const { createProduct,  getProductList, getProduct, deleteProduct, updateProduct } = require('../services/productServices');
+const { createProduct, getProductList, getProduct, deleteProduct, updateProduct } = require('../services/productServices');
+const permission = require('./middlewares/permission'); // 유저인증 & 권한 체크
 const router = express.Router();
 
 router.get('/:id', getProduct) // 상품 조회
 router.get('/', getProductList) // 상품 목록 조회
 
-router.post('/:id', createProduct) // 상품 추가
-router.put('/:id', updateProduct) // 상품 수정
-router.delete('/:id', deleteProduct) // 상품 삭제
+router.post('/:id', permission('admin'), createProduct) // 상품 추가
+router.put('/:id', permission('admin'), updateProduct) // 상품 수정
+router.delete('/:id', permission('admin'), deleteProduct) // 상품 삭제
 
 module.exports = router;

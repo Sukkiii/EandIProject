0788b4c7b74054b04b59b7ff415c5a021bd629a9
[SW7] 서원식 <seodnjstlr@gmail.com>
2023-11-09 08:08:08 +0000

    Update categories.js

--- routes/categories.js
+++ routes/categories.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { getCategory, createCategory, updateCategory, deleteCategory } = require('../services/categoryServices')
+const permission = require('./middlewares/permission'); // 유저인증 & 권한 체크
 const router = express.Router();
 
 router.get('/', getCategory) // 카테고리 조회
-router.post('/:id', createCategory) // 카테고리 추가
-router.put('/:id', updateCategory) // 카테고리 수정
-router.delete('/:id', deleteCategory) // 카테고리 삭제
+router.post('/:id', permission('admin'), createCategory) // 카테고리 추가
+router.put('/:id', permission('admin'), updateCategory) // 카테고리 수정
+router.delete('/:id', permission('admin'), deleteCategory) // 카테고리 삭제
 
 module.exports = router;

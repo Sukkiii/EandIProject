fc1f76608bfc027e6ea814a008b00dd57394ea52
wonsik <seodnjstlr@gmail.com>
2023-11-09 21:56:39 +0900

    category api add

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,7 @@ router.post('/login', login) // 로그인
 router.delete('/logout', logout) // 로그아웃
 
 //메인 페이지 및 상품 조회(비회원 기능)
-router.get('/categories/:id',getProductByTopCategory) // 상위 카테고리 상품목록
+router.get('/categories/:id',getProductByTopCategory) // 상위 카테고리 ->상품목록
 router.get('/categories/:id1/:id2',getProductsByCategory) // 상위 -> 하위 카테고리 상품목록
 router.get('/', getProductList) // 상품 목록 조회
 router.get('/produts/:id', getProduct) // 상품 조회

eba6ad74f8c9aeaeebb3e31e26c7e53cceb6d8df
[SW7] 서원식 <seodnjstlr@gmail.com>
2023-11-10 21:57:54 +0000

    Update orders.js

--- routes/orders.js
+++ routes/orders.js
@@ -8,7 +8,9 @@ router.delete('/admin/:id', permission('admin'), deleteOrder) // 주문 삭제
 router.put('/admin/:id', permission('admin'), updateDeliveryStatus) // 배송 상태 수정
 router.get('/admin', permission('admin'), getOrderList) // 전체 주문 목록 조회
 
-router.get('/:id', permission('user'), getOrder) // 주문 목록
-router.post('/:id', permission('user'), createOrder) // 주문 추가(완료)
+router.get('/', permission('user'), getOrders) // 주문 목록
+router.post('/', permission('user'), createOrder) // 주문 추가(완료)
 router.put('/:id', permission('user'), updateOrder) // 주문 수정
+router.get('/:id', permission('user'), getOrder) // 주문 상세
+
 module.exports = router;

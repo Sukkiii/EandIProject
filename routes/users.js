c2fc234cd68eebcd93a6f215b4a30b1e93da60f9
[SW7] 서원식 <seodnjstlr@gmail.com>
2023-11-10 19:46:38 +0000

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const { getUserList, updateUser, deleteUser, getUser, resignUser } = require('..
 const permission = require('../middlewares/permission'); // 유저인증 & 권한 체크
 const router = express.Router();
 
-router.get('/mypage/:id', permission('user'), getUser) // 회원 정보 조회
+router.get('/mypage', permission('user'), getUser) // 회원 정보 조회
 router.put('/mypage/:id', permission('user'), updateUser) // 회원 정보 수정
 router.delete('/mypage/:id', permission('user'), resignUser) // 회원 삭제(탈퇴)
 
